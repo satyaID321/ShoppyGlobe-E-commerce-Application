40d50a6a4762053224109cb973f95d7dbdadf7d9
satyaID321 <satyabhushanjaiswar@gmail.com>
2026-04-20 22:04:09 +0530

    store path fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./Redux/Store";
+import store from "./redux/store";
 import App from "./App";
 import "./index.css";
 
